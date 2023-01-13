385849f4ccff7fb65d7e467fdeffcfb505a1d984
wilguille <willyguille97@gmail.com>
2023-01-13 19:38:27 +0000

    Added questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        question: "What is the correct syntax for creating a variable in JavaScript?",
+        choices: ["var x = 5", "let x = 5", "const x = 5", "x = 5"],
+        answer: "var x = 5"
+    },
+    {
+        question: "What is the output of the following JavaScript code: console.log(typeof(null))?",
+        choices: ["undefined", "null", "object", "Error"],
+        answer: "object"
+    },
+    {
+        question: "What is the difference between == and ===?",
+        choices: ["== compares values, === compares values and types", "== compares types, === compares values", "There is no difference between == and ===", "== is for strings, === is for numbers"],
+        answer: "== compares values, === compares values and types"
+    },
+    {
+        question: "Which of the following is not a type of loop in JavaScript?",
+        choices: ["for", "while", "do-while", "foreach"],
+        answer: "foreach"
+    },
+    {
+        question: "What is the purpose of the 'this' keyword in JavaScript?",
+        choices: ["To reference the current object", "To reference the global object", "To reference the parent object", "To reference the function scope"],
+        answer: "To reference the current object"
+    }
+];
