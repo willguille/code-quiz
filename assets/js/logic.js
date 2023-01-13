a16a14c53f5e6be0798ec312f63af4e00c6d71de
wilguille <willyguille97@gmail.com>
2023-01-13 19:54:39 +0000

    Created function showQuestions to display the questions and answers on the screen, creating buttons for each answers and an event listener on click

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -22,3 +22,18 @@ function startQuiz() {
     }, 1000);
     showQuestion(currentQuestion);
 }
+// Function that shows the next question, taking them from the array in questions.js
+function showQuestion(questionIndex) {
+    var question = questions[questionIndex];
+    document.getElementById("question-title").textContent = question.question;
+    var choices = question.choices;
+    var choicesContainer = document.getElementById("choices");
+    choicesContainer.innerHTML = "";
+    for (var i = 0; i < choices.length; i++) {
+        var button = document.createElement("button");
+        button.classList.add("choice");
+        button.textContent = choices[i];
+        button.addEventListener("click", checkAnswer);
+        choicesContainer.appendChild(button);
+    }
+}
